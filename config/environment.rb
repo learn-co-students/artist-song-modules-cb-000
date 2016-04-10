caf06b355b2a3c486752e0bf5b3b7d4097e15522
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2016-04-10 11:29:00 -0400

    Update environment.rb
    require lib/song and lib/artist in environment file.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
-# require_relative '../lib/findable'
-# require_relative '../lib/memorable'
-# require_relative '../lib/paramable'
+require_relative '../lib/artist.rb'
+require_relative '../lib/song.rb'
+
+# require_relative '../lib/concerns/findable'
+# require_relative '../lib/concerns/memorable'
+# require_relative '../lib/concerns/paramable'
