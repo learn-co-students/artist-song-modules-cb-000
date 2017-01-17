e47ad36e3ec337e034965abe686729c65c452297
Kyle Greene <kgreene06@gmail.com>
2017-01-17 08:24:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
 
 class Song
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods 
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
@@ -18,17 +23,7 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def self.all
     @@songs
