9bb685a556b3b77a46d7dec0d967b5fc3519ed90
John Molina <jrmolin90@gmail.com>
2017-04-24 20:19:32 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 require 'pry'
-
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
-
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
