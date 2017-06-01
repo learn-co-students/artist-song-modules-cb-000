7728cfdaf62ece6c5f06629d2269c8afbd5a1991
Alyssa Hogan <alyssabhogan@gmail.com>
2017-06-01 13:48:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,11 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
   def self.find_by_name(name)
@@ -11,20 +16,12 @@ class Artist
   end
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
@@ -34,10 +31,6 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,12 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
-  def initialize
-    @@songs << self
-  end
+  @@songs = []
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -16,18 +17,6 @@ class Song
 
   def artist=(artist)
     @artist = artist
-  end
-
-  def self.reset_all
-    @@songs.clear
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
   end
 
   def self.all
