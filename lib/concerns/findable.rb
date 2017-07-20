308fc07593f039040f2d7e93ef1d6bdcf2bb40ef
theboymo <billfero@gmail.com>
2017-07-20 20:21:23 +0100

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,10 @@
+module Findable
+
+  module ClassMethods
+
+    def find_by_name(name)
+      self.all.detect {|s| s.name == name}
+    end
+  end
+
+end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,11 @@
+module Paramable
+
+  module InstanceMethods
+
+    def to_param
+      self.name.downcase.gsub(' ', '-')
+    end
+
+  end
+
+end
