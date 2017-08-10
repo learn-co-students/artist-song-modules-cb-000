937d1a45bb6daaa5c4634506c8239c03f0a650d2
JD Leshan <stritch@gmail.com>
2017-08-10 19:34:03 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,6 @@ require 'pry'
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
-require_relative '../lib/concerns/tracking'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
