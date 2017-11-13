9eb8902069b00e3f69106f514400ad2ac1e1ded8
Micah Ortega <classytattoo@hotmail.com>
2017-11-13 03:19:02 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,6 @@ require 'pry'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
---
+++ lib/concerns/paramble.rb
@@ -0,0 +1,7 @@
+module Paramble
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
+  end
+end
