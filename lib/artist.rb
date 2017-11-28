9d9900a02d282cfa238a304876cffdc4f5d1bb6e
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-28 23:55:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,14 +4,19 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@artists.detect{|a| a.name == name}
+  #end
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
@@ -19,25 +24,25 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#  def self.reset_all
+#    self.all.clear
+#  end
 
-  def self.count
-    @@artists.count
-  end
+#  def self.count
+#    @@artists.count
+#  end
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 
 end
---
+++ lib/concerns/findable.rb
@@ -0,0 +1,9 @@
+module Findable
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect{|o| o.name}
+    end
+
+  end
+
+end
