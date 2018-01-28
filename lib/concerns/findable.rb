87f943e63f5e071174bfd904018986539f09935d
Jen Poole <contact.vandepool@gmail.com>
2018-01-28 23:30:49 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,6 @@
+module Findable
+  module ClassMethods
+    def find_by_name
+    end
+  end
+end
--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,6 +1,5 @@
 module Memorable
   module ClassMethods
-
     def reset_all
       self.all.clear
     end
