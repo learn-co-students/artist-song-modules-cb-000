f346be8eb7e6f339cee018f1830b79c8bf67b36f
Kyle Harris <k_harris95@live.com>
2018-04-02 21:22:11 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,34 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
 class Artist
-  extend Memorable::ClassMethtods
-  extend Memorable::InstanceMethods
-  include Paramable
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def initialize
-    self.class.all << self
-    @songs = []
-  end
 
   def self.all
     @@artists
   end
 
+  def initialize
+    super
+    @songs = []
+  end
+
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-#  def to_param
-#    name.downcase.gsub(' ', '-')
-#  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,26 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/findable'
 class Song
-  extend Memorable::ClassMethtods
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+  def self.all
+    @@songs
   end
 
   def artist=(artist)
     @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 
-#  def to_param
-#    name.downcase.gsub(' ', '-')
-#  end
-
-  def self.all
-    @@songs
-  end
 end
